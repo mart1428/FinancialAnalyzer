f8ba65a2ceeb857d3ee9076318b08442aaef16a8
Christopher Martin <mart1428@users.noreply.github.com>
2021-01-07 21:11:32 +0700

    Completed Vertical Analysis on Analyzer.py

--- Analyzer.py
+++ Analyzer.py
@@ -12,15 +12,38 @@ class StockAnalyzer(YFinanceScrapper):
             Do vertical analysis on each fiscal year report
         '''
         va_istatement = {}
-        print(self.istatement_data)
         for k,v in self.istatement_data.items():
             va_list = []
             for i in range(len(v)):
                 va_list.append((self.istatement_data[k][i]/self.istatement_data['Total Revenue'][i]) * 100)
-            va_istatement[k] = self.istatement_data[k]
+            va_istatement[k] = va_list
 
-            print(va_list)
+        va_bsheet = {}
+        print(self.bsheet_data)
+        switch1 = False
+        switch2 = False
+        for k,v in self.bsheet_data.items():
+            va_list = []
+            if k != 'Accounts Payable' and not switch1 and not switch2:
+                for i in range(len(v)):
+                    va_list.append((self.bsheet_data[k][i]/self.bsheet_data['Total Assets'][i]) * 100)
+                va_bsheet[k] = va_list
+                switch1 = False
+            elif k != 'Common Stock' and not switch2:
+                for i in range(len(v)):
+                    va_list.append((self.bsheet_data[k][i]/self.bsheet_data['Total Liabilities'][i]) * 100)
+                va_bsheet[k] = va_list
+                switch1 = True
+                switch2 = False
+            else:
+                for i in range(len(v)):
+                    va_list.append((self.bsheet_data[k][i]/self.bsheet_data['Total liabilities and stockholders\' equity'][i]) * 100)
+                va_bsheet[k] = va_list
+                switch2 = True
 
+        print(va_bsheet)
+            
+                
 
 
 #==========================================
